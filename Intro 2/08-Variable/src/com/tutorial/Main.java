86fac4647363d0160b59e9e1b7d173c53b864538
stmikserver <yanyansofiyan89@outlook.com>
2020-01-07 10:35:18 +0700

    🧹

--- Intro 2/08-Variable/src/com/tutorial/Main.java
+++ Intro 2/08-Variable/src/com/tutorial/Main.java
@@ -2,7 +2,7 @@ package com.tutorial;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(final String[] args){
 
         // kita akan membuat variabel
         // tipe data
